b134e5cee4397cb2d83c1ca3703414967937ca40
thesagarmahajan <sagarmahajan31997@gmail.com>
2022-02-11 16:59:30 +0530

    Python MySQL Connector installed and SELECT query completed

--- src/models/user_model.py
+++ src/models/user_model.py
@@ -1,3 +1,12 @@
+
+import mysql.connector
+import json
 class user_model():
+    def __init__(self):
+        self.db = mysql.connector.connect(host="localhost",user="root",password="",database="test")
+        self.cur = self.db.cursor(dictionary=True)
+        
     def all_user_model(self):
-        return "This is all User Model"
+        self.cur.execute("SELECT * FROM users")
+        result = self.cur.fetchall()
+        return json.dumps(result)
